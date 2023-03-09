c0ae0026a60b64be172a01bb03c9d001f07d0e10
lh99j <grand2181@gmail.com>
2023-03-09 18:07:38 +0900

    2023-03-09 고급웹 수업

---
+++ 02/app.js
@@ -0,0 +1,82 @@
+/*
+    여
+    러
+    줄
+    주
+    석
+*/
+// 변수선언
+var title = "abc"
+
+console.log(title)
+
+// number
+var count = 5
+// string
+var count = "5"
+
+var nick = "AJ"
+var nick = "AJ"
+
+var isMan = true
+var isMan = false
+
+var isWoman = true
+var isWoman = false
+
+var log = function(str){
+    console.log(str)
+}
+
+function log(str){
+    console.log(str)
+}
+
+log(nick)
+
+var image = {}
+
+image.width = 100
+image.height = 100
+
+console.log(image)
+console.log(image.height)
+console.log(image['width'])
+
+image['width'] = 1
+
+console.log(image['width'])
+
+var test = { alt : "babo" }
+console.log(test.alt)
+
+var image = ['1.jpg', '2.jpg', 2, 3, true]
+
+image[0] = '3.jpg'
+
+console.log(image[0])
+
+var test = []
+test[0] = 1
+test[3] = 2
+
+var nick = 'aj'
+
+if(nick) console.log('hi~' + nick)
+
+var isNick = Boolean(nick)
+
+console.log(isNick)
+
+if(isNick) console.log('hi~' + nick)
+
+if(!!nick) console.log('hi~' + nick)
+
+// typeof는 자주 사용함
+typeof nick
+
+var arr = [1, 2, 3, 4, 5]
+
+for(var i = 0; i < arr.length - 1; i++){
+    console.log(arr[i])
+}
---
+++ 02/array.js
@@ -0,0 +1,64 @@
+var words = ['a', 'b', 'c'];
+
+//스택같이 꺼내 값 반환
+console.log(words.pop(), words);
+
+words.push("d")
+
+console.log(words)
+
+console.log(words.join("-"))
+
+console.log(words)
+
+var num = [1, 2, 3]
+
+console.log(words.concat(num))
+
+console.log(words)
+
+var words = ["a", "b", "c"]
+console.log(words.unshift("z"))
+
+console.log(words)
+
+words[1] = 1
+
+console.log(words)
+
+var newWords = words.unshift("z")
+
+console.log(newWords, words)
+
+var words = ["a", "b", "c"]
+console.log(words.slice(2))
+console.log(words.slice(0, 1))
+console.log(words)
+
+words.forEach(function(word, i, words){
+    console.log(word, i, words)
+})
+
+var words = ["A", "B", "C"]
+
+var newWords = words.map(function(word){
+    return word + "ok"
+})
+
+var nums = [1, 2, 3, 4, 5]
+var newNums = []
+for(var i = 0; i < nums.length; i++){
+    if(nums[i] > 3){
+        newNums.push(nums[i])
+    }
+}
+
+console.log(newNums)
+
+var newNums = nums.filter(function(num){
+    return num >= 3
+}).map(function(num){
+    return num + 10
+})
+
+console.log(newNums)
