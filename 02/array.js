57d5067e722fe924f0c406401834995587a694d0
lh99j <grand2181@gmail.com>
2023-03-16 16:06:16 +0900

    array.js 수정

--- 02/array.js
+++ 02/array.js
@@ -1,64 +1,76 @@
-var words = ['a', 'b', 'c'];
+// var words = ['a', 'b', 'c'];
 
-//스택같이 꺼내 값 반환
-console.log(words.pop(), words);
+// //스택같이 꺼내 값 반환
+// console.log(words.pop(), words);
 
-words.push("d")
+// words.push("d")
 
-console.log(words)
+// console.log(words)
 
-console.log(words.join("-"))
+// console.log(words.join("-"))
 
-console.log(words)
+// console.log(words)
 
-var num = [1, 2, 3]
+// var num = [1, 2, 3]
 
-console.log(words.concat(num))
+// console.log(words.concat(num))
 
-console.log(words)
+// console.log(words)
 
-var words = ["a", "b", "c"]
-console.log(words.unshift("z"))
+// var words = ["a", "b", "c"]
+// console.log(words.unshift("z"))
 
-console.log(words)
+// console.log(words)
 
-words[1] = 1
+// words[1] = 1
 
-console.log(words)
+// console.log(words)
 
-var newWords = words.unshift("z")
+// var newWords = words.unshift("z")
 
-console.log(newWords, words)
+// console.log(newWords, words)
 
-var words = ["a", "b", "c"]
-console.log(words.slice(2))
-console.log(words.slice(0, 1))
-console.log(words)
+// var words = ["a", "b", "c"]
+// console.log(words.slice(2))
+// console.log(words.slice(0, 1))
+// console.log(words)
 
-words.forEach(function(word, i, words){
-    console.log(word, i, words)
-})
+// words.forEach(function(word, i, words){
+//     console.log(word, i, words)
+// })
 
-var words = ["A", "B", "C"]
+// var words = ["A", "B", "C"]
 
-var newWords = words.map(function(word){
-    return word + "ok"
-})
+// var newWords = words.map(function(word){
+//     return word + "ok"
+// })
 
-var nums = [1, 2, 3, 4, 5]
-var newNums = []
-for(var i = 0; i < nums.length; i++){
-    if(nums[i] > 3){
-        newNums.push(nums[i])
-    }
-}
+// var nums = [1, 2, 3, 4, 5]
+// var newNums = []
+// for(var i = 0; i < nums.length; i++){
+//     if(nums[i] > 3){
+//         newNums.push(nums[i])
+//     }
+// }
 
-console.log(newNums)
+// console.log(newNums)
 
-var newNums = nums.filter(function(num){
-    return num >= 3
-}).map(function(num){
-    return num + 10
-})
+// var newNums = nums.filter(function(num){
+//     return num >= 3
+// }).map(function(num){
+//     return num + 10
+// })
 
-console.log(newNums)
+// console.log(newNums)
+
+// var month = ['January', 'February', 'March', 'April', 'May', 'June', 'August', 'September', 'October', 'November', 'December'];
+// console.log(month)
+
+var student = {}; 
+
+student.height = 174;
+student.weight = 80;
+
+student['name'] = 'lh99j';
+
+console.log(student)
