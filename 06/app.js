e6a81bf3b4732244fac8e7fffe53ea7a3958bf4f
lh99j <grand2181@gmail.com>
2023-04-11 13:27:33 +0900

    google form

---
+++ 06/app.js
@@ -0,0 +1,23 @@
+// const user = "lh99j";
+// const url = `https://github.com/${user}/start-fe`;
+
+// console.log(url);
+
+
+// const sayWhat = (what) => {
+//     console.log(what);
+//   };
+
+// sayWhat("Lim hyungjun");
+
+let student = {
+    studentName: "lhj",
+    grade: 4,
+    major: "컴퓨터공학전공"
+  };
+
+let {nastudentNameme, grade, major} = student;
+
+console.log(studentName);
+console.log(grade);
+console.log(major);
