be01427831ceb62bcf5f7f2a10ef2d1fbc12576c
lh99j <grand2181@gmail.com>
2023-04-19 15:58:28 +0900

    수정

--- 07/http/http.js
+++ 07/http/http.js
@@ -1,9 +1,9 @@
 fetch('https://jsonplaceholder.typicode.com/posts/1')  
 .then(response => response.json())
 .then(json => {
-    // console.log(json);
+    console.log(json);
 
-    // const { title } = json;
+    const { title } = json;
 
-    // document.getElementById('title').innerHTML = title;
+    document.getElementById('title').innerHTML = title;
 })
